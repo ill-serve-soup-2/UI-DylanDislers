534d9664f3160a3d5788e06b1a407a42cd8297dd
dislersd <dislersd@gmail.com>
2019-02-04 15:13:14 -0800

    added close event on nav menu

--- js/main.js
+++ js/main.js
@@ -2,11 +2,18 @@ class MenuToggle {
   constructor(element) {
     this.element = element;
     this.toggle = this.element.querySelector('.menuToggle')
+    this.closeMenu = this.element.querySelector('.close')
     this.navMenu = this.element.querySelector('.navMenu')
     this.toggle.addEventListener('click', () => this.popUp())
+    this.closeMenu.addEventListener('click', () => this.close())
   }
   popUp() {
     this.navMenu.classList.toggle('reveal')
+    this.toggle.style.display = 'none';
+  }
+  close() {
+    this.navMenu.classList.toggle('reveal')
+    this.toggle.style.display = 'flex';
   }
 }
 
